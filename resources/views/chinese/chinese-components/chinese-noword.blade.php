6091cf192293d5e3c8c39f22ea75af5a2e62a7a8
onlysuuji <onlysuuji@outlook.jp>
2024-10-15 20:59:55 +0900

    b

--- resources/views/chinese/chinese-components/chinese-noword.blade.php
+++ resources/views/chinese/chinese-components/chinese-noword.blade.php
@@ -4,7 +4,7 @@
         <div class=" gap-2 flex flex-row items-center ring-2 text-blue-900 rounded-xl text-xl ring-offset-8 hover:ring-blue-300   hover:bg-blue-50 hover:ring-offset-blue-50">
             <i class="fas fa-plus text-blue-700"></i>
             <div class="font-medium">
-            覚える単語を追加
+            覚える単語を追加aaaaaaaaabbbbbb
             </div>
         </div>
     </a>
--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
